f5827390861ce9290ba69acc8a80f802d02d6907
Daniel Vartanov <daniel.vartanov@deliveroo.co.uk>
2024-02-05 13:05:15 +0000

    Start switching to 1e7 everywhere

--- 2-1-innocent-refactoring.rb
+++ 2-1-innocent-refactoring.rb
@@ -8,7 +8,7 @@ def write_to_bank_account(value)
   @bank_account = value
 end
 
-100.times.map do
+1000.times.map do
   Thread.new do
     10_000.times do
       value = read_from_bank_account() # <--
@@ -19,6 +19,6 @@ end
 end.each(&:join)
 
 print @bank_account
-puts @bank_account == 1_000_000 ?
+puts @bank_account == 10_000_000 ?
        "\e[32m CORRECT \e[0m" :
        "\e[31m ERROR \e[0m"
